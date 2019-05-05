8ed98817a27310c4dd88bbdc87afb2a4b810b281
Haydar Ali Ismail <haydar_ai@yahoo.com>
2019-05-05 12:20:52 +0200

    Add tbox-abox linkage script

---
+++ graph_linker.py
@@ -0,0 +1,293 @@
+import os
+from SPARQLWrapper import SPARQLWrapper
+
+
+class Graph_Linker():
+    def __init__(self, *args, **kwargs):
+        self.sparql = SPARQLWrapper(os.getenv('SPARQL_ENDPOINT'))
+
+        return super().__init__(*args, **kwargs)
+
+    def link_authors(self):
+        print('Linking authors...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?a rdf:type dblp:Author
+            }
+
+            WHERE {
+                SELECT DISTINCT ?a
+                WHERE {
+                    ?a dblp:write ?p .
+                    FILTER NOT EXISTS {
+                        ?a rdf:type dblp:Author
+                    }
+                }
+            }
+        """)
+        sparql.query()
+        print('Authors linked to https://dblp.org/ontologies/Author.')
+
+    def link_papers(self):
+        print('Linking papers...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX bibo: <http://purl.org/ontology/bibo/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?p rdf:type bibo:AcademicArticle
+            }
+
+            WHERE {
+                SELECT DISTINCT ?p
+                WHERE {
+                    ?a dblp:write ?p .
+                    FILTER NOT EXISTS {
+                        ?p rdf:type bibo:AcademicArticle
+                    }
+                }
+            }
+        """)
+        sparql.query()
+        print('Papers linked to http://purl.org/ontology/bibo/AcademicArticle.')
+
+    def link_reviewers(self):
+        print('Linking reviewers...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX bibo: <http://purl.org/ontology/bibo/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?s rdf:type dblp:Reviewer
+            }
+
+            WHERE {
+                SELECT DISTINCT ?s
+                WHERE {
+                    ?s dblp:writeReview ?r .
+                    ?r dblp:about ?p .
+                    ?p rdf:type bibo:AcademicArticle .
+                    FILTER NOT EXISTS {
+                        ?s rdf:type dblp:Reviewer
+                    }
+                }
+            }
+        """)
+        sparql.query()
+        print('Reviewers linked to https://dblp.org/ontologies/Reviewer.')
+
+    def link_schools(self):
+        print('Linking schools...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?s rdf:type dblp:School
+            }
+
+            WHERE {
+                SELECT DISTINCT ?s
+                WHERE {
+                    ?a dblp:affiliatedWith ?s .
+                    FILTER ( regex(str(?s), "/schools/" )) .
+                    FILTER NOT EXISTS {
+                        ?s rdf:type dblp:School
+                    }
+                }
+            }
+        """)
+        sparql.query()
+        print('Schools linked to https://dblp.org/ontologies/School.')
+
+    def link_journals(self):
+        print('Linking journals...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX dbo: <https://dbpedia.org/ontology/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?j rdf:type dbo:AcademicJournal
+            }
+
+            WHERE {
+                SELECT DISTINCT ?j
+                WHERE {
+                    ?p dblp:publishedIn ?j .
+                    FILTER ( regex(str(?s), "/journals/" )) .
+                    FILTER NOT EXISTS {
+                        ?j rdf:type dbo:AcademicJournal
+                    }
+                }
+            }
+        """)
+        sparql.query()
+        print('Journals linked to https://dbpedia.org/ontology/AcademicJournal.')
+
+    def link_conferences(self):
+        print('Linking conferences...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX dbo: <https://dbpedia.org/ontology/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?c rdf:type dbo:AcademicConference
+            }
+
+            WHERE {
+                SELECT DISTINCT ?c
+                WHERE {
+                    ?p dblp:publishedIn ?c .
+                    FILTER ( regex(str(?c), "/conf/" )) .
+                    FILTER NOT EXISTS {
+                        ?c rdf:type dbo:AcademicConference
+                    }
+                }
+            }
+        """)
+        sparql.query()
+        print('Conferences linked to https://dbpedia.org/ontology/AcademicConference.')
+
+    def link_random_open_access_journals(self):
+        print('Generating and linking random open access journals...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX dbo: <https://dbpedia.org/ontology/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?j rdf:type dblp:OpenAccessJournal
+            }
+
+            WHERE {
+                SELECT DISTINCT ?j
+                WHERE {
+                    ?j rdf:type dbo:AcademicJournal .
+                    FILTER NOT EXISTS {
+                        ?j rdf:type dblp:OpenAccessJournal .
+                        ?j rdf:type dblp:CloseAccessJournal
+                    }
+                }
+                ORDER BY RAND()
+                LIMIT 200
+            }
+        """)
+        sparql.query()
+        print('Open access journals generated and linked to https://dblp.org/ontologies/OpenAccessJournal.')
+
+    def link_random_close_access_journals(self):
+        print('Generating and linking random close access journals...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX dbo: <https://dbpedia.org/ontology/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?j rdf:type dblp:CloseAccessJournal
+            }
+
+            WHERE {
+                SELECT DISTINCT ?j
+                WHERE {
+                    ?j rdf:type dbo:AcademicJournal .
+                    FILTER NOT EXISTS {
+                        ?j rdf:type dblp:OpenAccessJournal .
+                        ?j rdf:type dblp:CloseAccessJournal
+                    }
+                }
+                ORDER BY RAND()
+                LIMIT 200
+            }
+        """)
+        sparql.query()
+        print('Close access journals generated and linked to https://dblp.org/ontologies/CloseAccessJournal.')
+
+    def link_algorithm_conferences(self):
+        print('Linking algorithm conferences...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX dbo: <https://dbpedia.org/ontology/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?c rdf:type dblp:AlgorithmConference
+            }
+
+            WHERE {
+                SELECT DISTINCT ?c
+                WHERE {
+                    ?c rdf:type dbo:AcademicConference .
+                    ?p dblp:publishedIn ?c .
+                    ?p dblp:keyword ?k .
+                    FILTER(str(?k) IN ('algorithm')) .
+                    FILTER NOT EXISTS {
+                        ?c rdf:type dblp:AlgorithmConference
+                    }
+                }
+            }
+        """)
+        sparql.query()
+        print('Algorithm conferences linked to https://dblp.org/ontologies/AlgorithmConference.')
+
+    def link_network_conferences(self):
+        print('Linking network conferences...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX dbo: <https://dbpedia.org/ontology/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                ?c rdf:type dblp:NetworkConference
+            }
+
+            WHERE {
+                SELECT DISTINCT ?c
+                WHERE {
+                    ?c rdf:type dbo:AcademicConference .
+                    ?p dblp:publishedIn ?c .
+                    ?p dblp:keyword ?k .
+                    FILTER(str(?k) IN ('network', 'networks', 'cloud', 'internet', 'wlans')) .
+                    FILTER NOT EXISTS {
+                        ?c rdf:type dblp:NetworkConference
+                    }
+                }
+            }
+        """)
+        sparql.query()
+        print(
+            'Network conferences linked to https://dblp.org/ontologies/NetworkConference.')
+
+    def link_database_conferences(self):
+        print('Linking database conferences...')
+        self.sparql.setQuery("""
+            PREFIX dblp: <https://dblp.org/ontologies/>
+            PREFIX dbo: <https://dbpedia.org/ontology/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            INSERT {
+                c rdf:type dblp:DatabaseConference
+            }
+
+            WHERE {
+                SELECT DISTINCT ?c
+                WHERE {
+                    ?c rdf:type dbo:AcademicConference .
+                    ?p dblp:publishedIn ?c .
+                    ?p dblp:keyword ?k .
+                    FILTER(str(?k) IN ('data', 'database', 'databases')) .
+                    FILTER NOT EXISTS {
+                        ?c rdf:type dblp:DatabaseConference
+                    }
+                }
+            }
+        """)
+        sparql.query()
+        print(
+            'Database conferences linked to https://dblp.org/ontologies/DatabaseConference.')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import argparse
 from dblp_loader import DBLP_Loader
 from abox_generator import ABox_Generator
+from graph_linker import Graph_Linker
 
 load_dotenv()
 
@@ -10,6 +11,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--parse', action='store_true')
     parser.add_argument('--generate', action='store_true')
+    parser.add_argument('--link', action='store_true')
     args = parser.parse_args()
 
     if args.parse:
@@ -45,3 +47,16 @@ if __name__ == "__main__":
         abox_generator.create_journal_paper_non_corresponding_authors()
         abox_generator.create_paper_citations()
         abox_generator.save()
+    elif args.link:
+        graph_linker = Graph_Linker()
+        graph_linker.link_authors()
+        graph_linker.link_papers()
+        graph_linker.link_reviewers()
+        graph_linker.link_schools()
+        graph_linker.link_journals()
+        graph_linker.link_conferences()
+        graph_linker.link_random_open_access_journals()
+        graph_linker.link_random_close_access_journals()
+        graph_linker.link_algorithm_conferences()
+        graph_linker.link_network_conferences()
+        graph_linker.link_database_conferences()
